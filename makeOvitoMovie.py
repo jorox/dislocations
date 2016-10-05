5106bdda72d1fb0d071cf7e29cbe2bb28678b8cc
Wassim Kassem <wk234832@dell455102>
2016-10-05 12:05:32 +0200

    add multi-viewport option

--- makeOvitoMovie.py
+++ makeOvitoMovie.py
@@ -11,6 +11,7 @@ from ovito.modifiers import *
 from ovito.data import *
 from ovito.vis import *
 from PyQt5 import QtCore
+import os
 
 # This python script is meant to be run with Ovitos to create 4 movies 
 # representing the 4 main windows of the ovito screen. The input data
@@ -22,29 +23,31 @@ pers = ArgumentParser()
 
 pers.add_argument("fin", help="Input file", metavar="File-IN")
 pers.add_argument("-r","--range", help="Range", nargs=3, type=int, metavar=("START","END","INCR"))
-pers.add_argument("-o","--output", help="Output File Name base", metavar="FOUT.DIM.avi")
-pers.add_argument("-c","--cna", help="CNA cutoff", type=float, metavar="CUTOFF")
+pers.add_argument("-o","--output",help="Output File Name base", metavar="FOUT.DIM.avi")
+pers.add_argument("-c","--cna",  help="CNA cutoff", type=float, metavar="CUTOFF")
 pers.add_argument("-t","--time", help="Total length of video in seconds",type=int,metavar="TIME")
+pers.add_argument("-v", "--vpcount", help="output all viewports", type=int, default=1)
 args = pers.parse_args()
 
 # find wild-card in filename
 iwild = args.fin.find("*")
 if iwild == -1:
-    print("Fatal Error: filename does not contain wild-card '*'")
-    exit
+    print("Warning: ONLY ONE FILE SPECIFIED")
+    args.range = [0,1,1]
+    
 print(args)
 # Load file source 
 node = import_file(args.fin)
 print("... "+str(node.source.source_path)+ " loaded")
-print("... "+str(node.source.num_frames)+" frames found")
+print("... "+str(node.source.num_frames)+" snapshots found")
 # Handle warnings
 if args.cna is None:                     #cna cutoff unspecified
-    print("-->warning CNA cutoff undefined, using default 3.86")
+    print("--> warning CNA cutoff undefined, using default 3.86")
     args.cna = 3.86
 if args.range is None:
     args.range = [0,node.source.num_frames,1]
-if args.range and args.range[1]>node.source.num_frames: #range is too high
-    print("-->warning range specified too large")
+if args.range[1]>node.source.num_frames: #range is too high
+    print("-->warning range specified too large, respecifying...")
     args.range[1] = node.source.num_frames
 if args.output is None:
     args.output = "mov"
@@ -64,7 +67,7 @@ print("... modifiers added")
 #----------------------------------------------------------
 load = ["\\","|","/","-"]
 N = (args.range[1]-args.range[0])/args.range[2]  #number of video frames
-print("...%1.0d video frames\n    processing:"%(N))
+print("... %1.0d video frames\n    processing:"%(N))
 for i in range(args.range[0],args.range[1],args.range[2]):
     node.compute(i)
     prcnt = float(i)/N*100
@@ -79,40 +82,51 @@ cell = node.source.cell
 cell.display.enabled = True
 cell.display.rendering_color = (1,1,1)
 
+for ivp in range(args.vpcount):
+    vp = Viewport()
+    if ivp == 0:
+        vp.type = Viewport.Type.PERSPECTIVE
+        vp.camera_pos = (-70, -500, 100)
+        vp.camera_dir = (0.2, 1, -0.25)
+    elif ivp ==1:
+        vp.type = Viewport.Type.FRONT
+        vp.zoom_all()
+    else:
+        vp.type = Viewport.Type.TOP
+        vp.zoom_all()
 
-
-vp = Viewport()
-vp.type = Viewport.Type.PERSPECTIVE
-vp.zoom_all()
-vp.camera_pos = (-100, -150, 150)
-vp.camera_dir = (2, 3, -3)
-vp.fov = math.radians(20.0)
-
+    tripod = CoordinateTripodOverlay()
+    tripod.size = 0.1
+    tripod.alignment = QtCore.Qt.AlignRight ^ QtCore.Qt.AlignBottom
+#vp.zoom_all()
+#vp.fov = math.radians(20.0)
 #Create an overlay.
-tripod = CoordinateTripodOverlay()
-tripod.size = 0.07
-tripod.alignment = QtCore.Qt.AlignRight ^ QtCore.Qt.AlignBottom
 
 # Attach overlay to the active viewport.
 #viewport = ovito.dataset.viewports.active_vp
 #viewport.overlays.append(tripod)
-vp.overlays.append(tripod)
+    vp.overlays.append(tripod)
 
-if args.time is not None:
-    fps = N/args.time
-else:
-    fps = 10
-ovito.dataset.anim.frames_per_second = fps
+    if args.time is not None:
+        fps = int(N/args.time)
+    else:
+        fps = 10
+    ovito.dataset.anim.frames_per_second = fps
 
-rs = RenderSettings(
-    filename = args.output+".avi",
-    size = (1024, 768),
-    background_color=(0,0,0),
-    custom_range = (args.range[0],args.range[1])
-)
-#rs.renderer = TachyonRenderer()
-rs.range = RenderSettings.Range.CUSTOM_INTERVAL
-print(rs.range)
-rs.renderer.antialiasing = True
-print("... rendering movie")
-vp.render(rs)
+    rs = RenderSettings(
+        filename = args.output+"."+str(vp.type)+".avi",
+        size = (1024, 768),
+        background_color=(0,0,0),
+        custom_range = (args.range[0],args.range[1])
+        )
+    #rs.renderer = TachyonRenderer()
+    if args.range[1]>1:
+        rs.range = RenderSettings.Range.CUSTOM_INTERVAL  #video
+    else:
+        rs.filename=args.output+"."+str(vp.type)+".png"                  #image
+
+    print(rs.range)
+    rs.renderer.antialiasing = True
+    print("... rendering movie: %s, %d fps, %s "%(str(vp.type),fps, str(rs.range)))
+    vp.render(rs)
+    print("... done writing to "+os.getcwd()+"/"+rs.filename)
